58b1e15388e421a84a308d4607c9bcb4d9654a11
CailleauThierry <CailleauThierry@users.noreply.github.com>
2016-05-04 10:30:44 +0200

    was Get-EVVersion02.ps1
    -  Edited private info out of Get-EVVersion02.ps1
    -  I was playing with Filtering and sorting the element I needed with
    .Split and Select-Object

--- Get-EVVersion.ps1
+++ Get-EVVersion.ps1
@@ -1,8 +1,14 @@
-﻿#opened "C:\hsgTest\projects\zip\1\zipoutput\Host-1-EXCH\BACKUP.XLOG" saved as clear text in "C:\hsgTest\input\BACKUP_filtered.XLOG"
+﻿# working "C:\hsgTest\projects\Get-EVVersion\Get-EVVersion02.ps1" with results:
+#HostName                    IPAddress                   TaskName                    VaultVersion               LogPath                   
+#--------                    ---------                   --------                    ------------               -------                   
+#Host-1                    192.168.1.1                 Host-1-EXCH               7.01                       C:\hsgTest\input\BACKUP...
+
+
+#opened "C:\hsgTest\projects\zip\1\zipoutput\Host-1-EXCH\BACKUP.XLOG" saved as clear text in "C:\hsgTest\input\BACKUP_filtered.XLOG"
 # $log1 for Get-Content of it
 $log1 = Get-Content C:\hsgTest\input\BACKUP_filtered.XLOG
 
-#PS C:\Users\tcailleau> $log1 | Where-Object {$_.Contains("hn=") -eq $true} | ForEach-Object {$_.Split(', ')}
+#PS C:\Users\tcailleau> $log1 | Select-Object {$_.Contains("hn=") -eq $true} | ForEach-Object {$_.Split(', ')} | Where-Object {$_.Contains("hn=") -eq $true}
 #04-Dec
 #21:30:40
 #UTIL-I-05210
@@ -107,12 +113,114 @@ $sys1 | Add-Member NoteProperty HostName "-"
 $sys1 | Add-Member NoteProperty IPAddress "-.-.-.-"
 $sys1 | Add-Member NoteProperty TaskName "-"
 $sys1 | Add-Member NoteProperty VaultVersion "-.-"
-$sys1 | Add-Member NoteProperty TaskID "-"
+# $sys1 | Add-Member NoteProperty TaskID "-" # TaskID is not defined on Agent side. This will become relevant for the Vault logs
 $sys1 | Add-Member NoteProperty LogPath "C:\-"
 
 # Use case
-$sys1 | ft *
+#$sys1 | ft *
 #HostName                      IPAddress                     TaskName                      VaultVersion                  TaskID                        LogPath                      
 #--------                      ---------                     --------                      ------------                  ------                        -------                      
 #
 #-                             -.-.-.-                       -                             -.-                           -                             C:\-                 
+
+$sys1.LogPath = $log1[0].PSPath
+
+#PS C:\windows\system32> $sys1 | gm
+#
+#
+#   TypeName: System.Management.Automation.PSCustomObject
+#
+#Name         MemberType   Definition                                                 
+#----         ----------   ----------                                                 
+#Equals       Method       bool Equals(System.Object obj)                             
+#GetHashCode  Method       int GetHashCode()                                          
+#GetType      Method       type GetType()                                             
+#ToString     Method       string ToString()                                          
+#HostName     NoteProperty System.String HostName=-                                   
+#IPAddress    NoteProperty System.String IPAddress=-.-.-.-                            
+#LogPath      NoteProperty System.String LogPath=C:\hsgTest\input\BACKUP_filtered.XLOG
+#TaskID       NoteProperty System.String TaskID=-                                     
+#TaskName     NoteProperty System.String TaskName=-                                   
+#VaultVersion NoteProperty System.String VaultVersion=-.-           
+
+$a = $log1 | Where-Object {$_.Contains("hn=") -eq $true} | ForEach-Object {$_.Split(', ')} | Where-Object {$_.Contains("hn=") -eq $true} | Sort-Object -Unique | ForEach-Object {$_.Split('hn=')} 
+
+#PS C:\windows\system32> $a
+#
+#
+#
+#Host-1
+
+$sys1.HostName = $a[-1]
+
+#PS C:\windows\system32> $sys1
+#
+#
+#HostName     : Host-1
+#IPAddress    : -.-.-.-
+#TaskName     : -
+#VaultVersion : -.-
+#TaskID       : -
+#LogPath      : C:\hsgTest\input\BACKUP_filtered.XLOG
+
+$a = $log1 | Where-Object {$_.Contains("hn=") -eq $true} | ForEach-Object {$_.Split(', ')} | Where-Object {$_.Contains("ip=") -eq $true} | Sort-Object -Unique | ForEach-Object {$_.Split('ip=')} 
+
+$sys1.IPAddress = $a[-1]
+
+#PS C:\windows\system32> $a = $log1 | Where-Object {$_.Contains("hn=") -eq $true} | ForEach-Object {$_.Split(', ')} | Where-Object {$_.Contains("ip=") -eq $true} | Sort-Object -Unique | ForEach-Object {$_.Split('ip=')} 
+#
+#PS C:\windows\system32> $a
+#
+#
+#
+#192.168.1.1
+#PS C:\windows\system32> $sys1.IPAddress = $a[-1]
+#PS C:\windows\system32> $sys1
+#
+#
+#HostName     : Host-1
+#IPAddress    : 192.168.1.1
+#TaskName     : -
+#VaultVersion : -.-
+#TaskID       : -
+#LogPath      : C:\hsgTest\input\BACKUP_filtered.XLOG
+
+$a = $log1 | Where-Object {$_.Contains("tn=") -eq $true} | ForEach-Object {$_.Split(', ')} | Where-Object {$_.Contains("tn=") -eq $true} | Sort-Object -Unique | ForEach-Object {$_.Split('tn=')} 
+$sys1.TaskName = $a[-1]
+
+#PS C:\windows\system32> $a = $log1 | Where-Object {$_.Contains("tn=") -eq $true} | ForEach-Object {$_.Split(', ')} | Where-Object {$_.Contains("tn=") -eq $true} | Sort-Object -Unique | ForEach-Object {$_.Split('tn=')} 
+#PS C:\windows\system32> $a
+#
+#
+#
+#Host-1-EXCH
+#PS C:\windows\system32> $sys1.TaskName = $a[-1]
+#PS C:\windows\system32> $sys1
+#
+#
+#HostName     : Host-1
+#IPAddress    : 192.168.1.1
+#TaskName     : Host-1-EXCH
+#VaultVersion : -.-
+#TaskID       : -
+#LogPath      : C:\hsgTest\input\BACKUP_filtered.XLOG
+
+$a = $log1 | Where-Object {$_.Contains(" Vault Version ") -eq $true} | Sort-Object -Unique | ForEach-Object {$_.Split(' Vault Version ')} 
+$sys1.VaultVersion = $a[-1]
+
+# one difference is there is no need to split the Vault Version line
+#PS C:\windows\system32> $a = $log1 | Where-Object {$_.Contains(" Vault Version ") -eq $true} | Sort-Object -Unique | ForEach-Object {$_.Split(' Vault Version ')} 
+#
+#PS C:\windows\system32> $sys1.VaultVersion = $a[-1]
+#PS C:\windows\system32> $sys1
+#
+#
+#HostName     : Host-1
+#IPAddress    : 192.168.1.1
+#TaskName     : Host-1-EXCH
+#VaultVersion : 7.01
+#LogPath      : C:\hsgTest\input\BACKUP_filtered.XLOG
+
+
+# Use case
+$sys1 | ft *

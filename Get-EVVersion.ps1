68befc81dd3d6e4df21b3a378fbc36bccf673c84
CailleauThierry <CailleauThierry@users.noreply.github.com>
2016-05-20 06:52:47 +0200

    was Get-EVVersion06_02.ps1
    Get-EVVersion06_02.ps1 show a bit more structure as to where it came
    from i.e. Get-EVVersion06_01.ps1. This higlight the need for version
    control I already knew about. Here I set stronger requirements for my
    input

--- Get-EVVersion.ps1
+++ Get-EVVersion.ps1
@@ -1,4 +1,4 @@
-﻿# working "C:\hsgTest\projects\Get-EVVersion\Get-EVVersion06_01.ps1" to get tid and paste results to clipboard
+﻿#working "C:\hsgTest\projects\Get-EVVersion\Get-EVVersion06_02.ps1" with input parameter an Agent log in txt format (save as txt from logviewer) and paste results to clipboard
 
 param ( 
 [Parameter(mandatory=$true)][string] $InputFile
@@ -8,8 +8,8 @@ param (
 # That was because I had to select "Show PowerShell Console" when crearing the executable with PowerGUI
 
 
-# "C:\hsgTest\projects\Get-EVVersion\Get-EVVersion06_01.ps1" based on working
-# "C:\hsgTest\projects\Get-EVVersion\Get-EVVersion04_01.ps1" (- some Comments) with results:
+# "C:\hsgTest\projects\Get-EVVersion\Get-EVVersion06_02.ps1" based on working
+# "C:\hsgTest\projects\Get-EVVersion\Get-EVVersion06_01.ps1" tid to clipboard
 
 
 
@@ -85,23 +85,64 @@ end
 ##############################################################################
 
 
-# $log1 for Get-Content of it$log1 = Get-Content C:\hsgTest\input\BACKUP_filtered.XLOG
+# $log1 for Get-Content of it $log1 = Get-Content C:\hsgTest\input\Backup-526ABFBB-48AC-29B4.LOG
 
 $VaultLog = New-Object PSObject
-$VaultLog | Add-Member NoteProperty VaultName "-"
 $VaultLog | Add-Member NoteProperty LogPath "-"
 $VaultLog | Add-Member NoteProperty LogName "-"
+$VaultLog | Add-Member NoteProperty VaultName "-"
+$VaultLog | Add-Member NoteProperty VaultVersion "-"
+$VaultLog | Add-Member NoteProperty AgentHostname "-"
+$VaultLog | Add-Member NoteProperty AgentIP "-.-.-.-"
+$VaultLog | Add-Member NoteProperty AgentVersion "-"
+$VaultLog | Add-Member NoteProperty TaskName "-"
 $VaultLog | Add-Member NoteProperty TaskID "(-)"
+$VaultLog | Add-Member NoteProperty SafesetNumber "-"
+
 
 $log1 = Get-Content $InputFile
+$VaultLog.LogPath = $log1[1].PSPath 
+$VaultLog.LogName = $log1[1].PSChildName
 
-$VaultLog.LogPath = $log1[1].PSPath
+#VaultName
+$a = $log1 | Where-Object {$_ -match ("Vault: ") } | ForEach-Object {$_.Split(" ")}
+$VaultLog.VaultName = $a[-1]
 
-                                                                                                                                          
-$log1[1].PSChildName
+#VaultVersion
+$a = $log1 | Where-Object {$_ -match ("EVault Software Director Version ") } | ForEach-Object {$_.Split(" ")}
+
+#function to take the next value after "Version"
+$i = 0
+foreach ($element in $a){
+	$i++
+	if ($element.Contains("Version")){
+		$VaultLog.VaultVersion = $a[$i]
+	}
+}
+
+#AgentHostname
+$a = $log1 | Where-Object {$_ -match ("hn = ") } | ForEach-Object {$_.Split(" ")}
+$VaultLog.AgentHostname = $a[-1]
+
+#AgentIP
+$a = $log1 | Where-Object {$_ -match ("ip = ") } | ForEach-Object {$_.Split(" ")}
+$VaultLog.AgentIP = $a[-1]
+
+#AgentVersion
+$a = $log1 | Where-Object {$_ -match ("Agent version is ") } | ForEach-Object {$_.Split(" ")}
+$VaultLog.AgentVersion = $a[-1]
+
+#TaskName
+$a = $log1 | Where-Object {$_ -match ("tn = ") } | ForEach-Object {$_.Split(" ")}
+$VaultLog.TaskName = $a[-1]
+
+#TaskID
 $a = $log1 | Where-Object {$_ -match ("tid= ") } | ForEach-Object {$_.Split(" ")}
-
 $VaultLog.TaskID = $a[-1]
+
+#SafesetNumber
+$a = $log1 | Where-Object {$_ -match ("catalog number is ") } | ForEach-Object {$_.Split(" ")}
+$VaultLog.SafesetNumber = $a[-1]
 
 #$log1[1].PSChildName
 #$a = $log1 | Where-Object {$_ -match ("tid= ") }
@@ -120,8 +161,8 @@ $VaultLog | Set-Clipboard
 #LogPath      : C:\hsgTest\input\BACKUP_filtered.XLOG
 #AgentVersion : 7.24.3120
 #VaultVersion : 7.01
-#HostName     : TMTC-SBS
+#HostName     : Host-1
 #IPAddress    : 192.168.1.1
-#TaskName     : TMTC-SBS-EXCH
+#TaskName     : Host-1-EXCH
 #
 #

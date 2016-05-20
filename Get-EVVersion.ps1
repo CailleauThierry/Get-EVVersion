dc09632d1a383c17d6d930dd495cc6bc746160ba
CailleauThierry <CailleauThierry@users.noreply.github.com>
2016-05-20 06:22:34 +0200

    Was Get-EVVersion04.ps1
    working to paste results to clipboard

--- Get-EVVersion.ps1
+++ Get-EVVersion.ps1
@@ -1,9 +1,86 @@
-﻿# Get-EVVersion03_1.ps1 > took a while to get Agent version. Learned Select-String will help me optimize the whole function
+﻿# working to paste results to clipboard
 
-# "C:\hsgTest\projects\Get-EVVersion\Get-EVVersion03.ps1" based on working "C:\hsgTest\projects\Get-EVVersion\Get-EVVersion02.ps1" (- some Comments) with results:
-#HostName                    IPAddress                   TaskName                    VaultVersion               LogPath                   
-#--------                    ---------                   --------                    ------------               -------                   
-#Host-1                    192.168.1.1                 Host-1-EXCH               7.01                       C:\hsgTest\input\BACKUP...
+#Work in progress to investiage :  Get-EVVersion03_1.ps1 highlights a Select-String better approach
+# I did a focus in Get-EVVersion03_1.ps1 to Get AgentVersion working
+# Aslo a good idea would simply take the clipboard input and then launch exe to paste back into clipboard
+# "C:\hsgTest\projects\Get-EVVersion\Get-EVVersion04.ps1" based on working "C:\hsgTest\projects\Get-EVVersion\Get-EVVersion03.ps1" (- some Comments) with results:
+#LogPath                  AgentVersion             VaultVersion             HostName                 IPAddress                TaskName       
+#-------                  ------------             ------------             --------                 ---------                --------       
+#C:\hsgTest\input\BACK... 7.24.3120                7.01                     Host-1                 192.168.1.1              Host-1-EXCH 
+
+
+#beginning of#################################################################
+##
+## Set-Clipboard
+##
+## From Windows PowerShell Cookbook (O'Reilly)
+## by Lee Holmes (http://www.leeholmes.com/guide)
+##
+##############################################################################
+Function Set-Clipboard {
+<#
+
+.SYNOPSIS
+
+Sends the given input to the Windows clipboard.
+
+.EXAMPLE
+
+dir | Set-Clipboard
+This example sends the view of a directory listing to the clipboard
+
+.EXAMPLE
+
+Set-Clipboard "Hello World"
+This example sets the clipboard to the string, "Hello World".
+
+#>
+
+param(
+    ## The input to send to the clipboard
+    [Parameter(ValueFromPipeline = $true)]
+    [object[]] $InputObject
+)
+
+begin
+{
+    Set-StrictMode -Version Latest
+    $objectsToProcess = @()
+}
+
+process
+{
+    ## Collect everything sent to the script either through
+    ## pipeline input, or direct input.
+    $objectsToProcess += $inputObject
+}
+
+end
+{
+    ## Launch a new instance of PowerShell in STA mode.
+    ## This lets us interact with the Windows clipboard.
+    $objectsToProcess | PowerShell -NoProfile -STA -Command {
+        Add-Type -Assembly PresentationCore
+
+        ## Convert the input objects to a string representation
+        $clipText = ($input | Out-String -Stream) -join "`r`n"
+
+        ## And finally set the clipboard text
+        [Windows.Clipboard]::SetText($clipText)
+    }
+}
+}
+
+#end of#######################################################################
+##
+## Set-Clipboard
+##
+## From Windows PowerShell Cookbook (O'Reilly)
+## by Lee Holmes (http://www.leeholmes.com/guide)
+##
+##############################################################################
+
+
 
 $log1 = Get-Content C:\hsgTest\input\BACKUP_filtered.XLOG
 
@@ -21,150 +98,56 @@ $AgentLog | Add-Member NoteProperty TaskName "-"
 
 $AgentLog.LogPath = $log1[0].PSPath
 
+
+
 $a = $log1 | Where-Object {$_.Contains(" Agent Version ") -eq $true} | ForEach-Object {$_.Split(' ')} 
 
 $b = $a | Select-string -Pattern "Version" -Context 0,1 | Select-Object -Unique
 
+# Using Select-String was more efficient but extracting the result as a string took a bit of digging explaining the "$b.Context.PostContext)[0]"
+
 $AgentLog.AgentVersion = ($b.Context.PostContext)[0]
 
-$AgentLog | ft *
+$a = $log1 | Where-Object {$_.Contains(" Vault Version ") -eq $true} | Sort-Object -Unique | ForEach-Object {$_.Split(' Vault Version ')} 
+$AgentLog.VaultVersion = $a[-1]
 
-#for ($i=0; $i -le $a.Length; $i++) {
-#   $res = 
-#}
-
-# -------------------------- EXAMPLE 8 --------------------------
+# one difference is there is no need to split the Vault Version line
+#PS C:\windows\system32> $a = $log1 | Where-Object {$_.Contains(" Vault Version ") -eq $true} | Sort-Object -Unique | ForEach-Object {$_.Split(' Vault Version ')} 
 #
-# C:\PS>$f = select-string -path audit.log -pattern "logon failed" -context 2, 3
-#
-# C:\PS> $f.count
-#
-# C:\PS> ($f)[0].context | format-list
+#PS C:\windows\system32> $AgentLog.VaultVersion = $a[-1]
+#PS C:\windows\system32> $AgentLog
 #
 #
-# Description
-# -----------
-# The first command searches the Audit.Log file for the phrase "logon failed." It uses the Context parameter to capture 2 lines before the match and 3 lines after the match.
-# The second command uses the Count property of object arrays to display the number of matches found, in this case, 2.
+#HostName     : Host-1
+#IPAddress    : 192.168.1.1
+#TaskName     : Host-1-EXCH
+#VaultVersion : 7.01
+#LogPath      : C:\hsgTest\input\BACKUP_filtered.XLOG
+
+$a = $log1 | Where-Object {$_.Contains("hn=") -eq $true} | ForEach-Object {$_.Split(', ')} | Where-Object {$_.Contains("hn=") -eq $true} | Sort-Object -Unique | ForEach-Object {$_.Split('hn=')} 
+
+$AgentLog.HostName = $a[-1]
+
+$a = $log1 | Where-Object {$_.Contains("hn=") -eq $true} | ForEach-Object {$_.Split(', ')} | Where-Object {$_.Contains("ip=") -eq $true} | Sort-Object -Unique | ForEach-Object {$_.Split('ip=')} 
+
+$AgentLog.IPAddress = $a[-1]
+
+$a = $log1 | Where-Object {$_.Contains("tn=") -eq $true} | ForEach-Object {$_.Split(', ')} | Where-Object {$_.Contains("tn=") -eq $true} | Sort-Object -Unique | ForEach-Object {$_.Split('tn=')} 
+$AgentLog.TaskName = $a[-1]
+
+
+# Use case
+# $AgentLog | ft *
+$AgentLog | Set-Clipboard
+ 
+#Once pasted frm clipboard the result is:
 #
-#The third command displays the lines stored in the Context property of the first MatchInfo object. It uses array notation to indicate the first match (match 0 in a zero-based
-#array), and it uses the Format-List cmdlet to display the value of the Context property as a list.
 #
-#The output consists of two MatchInfo objects, one for each match detected. The context lines are stored in the Context property of the MatchInfo object.
-
-
-
-# -------------------------- EXAMPLE 6 --------------------------
-#
-# C:\PS>$a = get-eventlog -log "Windows PowerShell"
-#
-# C:\PS> $a | select-object -index 0, ($a.count - 1)
+#LogPath      : C:\hsgTest\input\BACKUP_filtered.XLOG
+#AgentVersion : 7.24.3120
+#VaultVersion : 7.01
+#HostName     : Host-1
+#IPAddress    : 192.168.1.1
+#TaskName     : Host-1-EXCH
 #
 #
-# Description
-# -----------
-# These commands get the first (newest) and last (oldest) events in the Windows Powershell event log.
-#
-# The first command uses the Get-EventLog cmdlet to get all events in the Windows Powershell log. It saves the events in the $a variable.
-## The second command uses a pipeline operator (|) to send the events in $a to the Select-Object cmdlet. The Select-Object command uses the Index parameter to select items by the
-# index number. The index for the first event is 0. The index for the last event is the number of items in $a minus 1.
-
-
-# $a | select ($_) > display each element of array $a
-
-# I would like to be ableto do a foreach ($res in $a) $_.Text -eq Version $res=$a[+1]  > I want to catch the next entry after version
-
-#$a | foreach {$a}  #there are 80 items in $a, it display all 80 items 80 time. It's like the first $a is just a counter
-
-#$a[0]
-#$a[1]
-#$a[2]
-#04-Dec
-#21:30:02
-#BKUP-I-04314
-
-# $a = $log1 | Where-Object {$_.Contains(" Agent Version ") -eq $true} | ForEach-Object {$_.Split(' ')} 
-#PS C:\Users\tcailleau\Documents> $a
-#04-Dec
-#21:30:02
-#BKUP-I-04314
-#Agent
-#Version
-#7.24.3120
-#Oct
-#24
-#2013
-#01:29:17
-#05-Dec
-#21:30:05
-#BKUP-I-04314
-#Agent
-#Version
-#7.24.3120
-#Oct
-#24
-#2013
-#01:29:17
-#06-Dec
-#21:30:07
-#BKUP-I-04314
-#Agent
-#Version
-#7.24.3120
-#Oct
-#24
-#2013
-#01:29:17
-#07-Dec
-#21:30:10
-#BKUP-I-04314
-#Agent
-#Version
-#7.24.3120
-#Oct
-#24
-#2013
-#01:29:17
-#09-Dec
-#21:30:18
-#BKUP-I-04314
-#Agent
-#Version
-#7.24.3120
-#Oct
-#24
-#2013
-#01:29:17
-#10-Dec
-#21:30:21
-#BKUP-I-04314
-#Agent
-#Version
-#7.24.3120
-#Oct
-#24
-#2013
-#01:29:17
-#11-Dec
-#21:30:26
-#BKUP-I-04314
-#Agent
-#Version
-#7.24.3120
-#Oct
-#24
-#2013
-#01:29:17
-#12-Dec
-#21:30:30
-#BKUP-I-04314
-#Agent
-#Version
-#7.24.3120
-#Oct
-#24
-#2013
-#01:29:17
-
-#$log1 | Where-Object {$_.Contains(" Agent Version ") -eq $true} | Sort-Object -Unique | ForEach-Object {$_.Split(' Agent Version ')} 
-#$AgentLog.AgentVersion = $a[-1]

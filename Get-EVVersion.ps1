0f735d6523e3ab160a13eba8c853cbf702a2847f
CailleauThierry <CailleauThierry@users.noreply.github.com>
2016-05-22 00:44:44 +0200

    was Get-EVVersion10_07.ps1
    worked on documentation regex refining and $Matches loop improvement

--- Get-EVVersion.ps1
+++ Get-EVVersion.ps1
@@ -1,26 +1,17 @@
 ﻿<#
 	.SYNOPSIS
-		Description still to come
-
+		Get-EVVersion10_07.ps1 is a powershell script that extracts essential ticket documenting information for Agent logs and places it in the clipboard (ready to paste into Salesforce or else)
 	.DESCRIPTION
-		Get-EVVersion10_06.ps1 based on processing Get-EVVersion10_05.ps1 which adds a timestamp. Trying to understand the workings of where-object + match
-		I want to add multiple reccurence of the same entry in a different PropertyName
+		Get-EVVersion10_07.ps1 based on working Get-EVVersion10_04.ps1 which adds the end of log time
 		It uses the desktop shortcut as the wrapper. 
-		
+		Also could add multiple reccurence of the same entry in a different PropertyName
 	.PARAMETER  ParameterA
-		The description of the ParameterA parameter.
-
-	.PARAMETER  ParameterB
-		The description of the ParameterB parameter.
-
-	.EXAMPLE
-		PS C:\> Get-Something -ParameterA 'One value' -ParameterB 32
-
-	.EXAMPLE
-		PS C:\> Get-Something 'One value' 32
+		Drag&Drop of the Agent log file to the script shotcut. It takes the full path
+	.EXAMPLE. 
+		Drag&Drop of the Agent log file to the script shotcut. It takes the full path
 
 	.INPUTS
-		Evault backup logs in text format
+		Evault backup/restore/synch logs in text format
 
 	.OUTPUTS
 		TypeName: System.Management.Automation.PSCustomObject
@@ -39,29 +30,29 @@
 
 
 param ( 
-[Parameter(mandatory=$false)][string] $InputFile  =  'C:\posh\input\BACKUP.XLOG.log' #  since I use the same file for testing , I should check against an expected output result
+[Parameter(mandatory=$false)][string] $InputFile = "C:\posh\input\SYNCH.XLOG.log"
 )
 
 
 # set Clipboard
 
-. C:\posh\projects\Clipboard\Set-Clipboard_fc.ps1
+. C:\posh\Set-Clipboard_fc.ps1
 
 
 # $log1 for Get-Content of it $log1 = Get-Content C:\hsgTest\input\Backup-526ABFBB-48AC-29B4.LOG
 
 $AgentLog = New-Object PSObject
-$AgentLog | Add-Member NoteProperty LogPath "C:\-"
-$AgentLog | Add-Member NoteProperty LogName "-"
-$AgentLog | Add-Member NoteProperty TimeStamp "dd-mmm hh:mm:ss"
-$AgentLog | Add-Member NoteProperty AgentVersion "-.--.----"
-$AgentLog | Add-Member NoteProperty VaultVersion "-.--"
-$AgentLog | Add-Member NoteProperty HostName "-"
-$AgentLog | Add-Member NoteProperty IPAddress "-.-.-.-"
-$AgentLog | Add-Member NoteProperty TaskName "-"
-$AgentLog | Add-Member NoteProperty TaskGUID "-"
-$AgentLog | Add-Member NoteProperty AgentGUID "-"
-$AgentLog | Add-Member NoteProperty VaultGUID "-"
+$AgentLog | Add-Member NoteProperty LogPath "A path to a valid Agent log file was not found"
+$AgentLog | Add-Member NoteProperty LogName "Could not find a file name in Dragged & Dropped input"
+$AgentLog | Add-Member NoteProperty LogEndTime "Could not find a valid time format for an Agent log in text format"
+$AgentLog | Add-Member NoteProperty AgentVersion "No Agent version Available in this log"
+$AgentLog | Add-Member NoteProperty VaultVersion "No Vault version Available in this log"
+$AgentLog | Add-Member NoteProperty HostName "Could not find a Host name"
+$AgentLog | Add-Member NoteProperty IPAddress "Could not find a IP Address"
+$AgentLog | Add-Member NoteProperty TaskName "Could not find a task name"
+$AgentLog | Add-Member NoteProperty TaskGUID "Could not find a Task GUID"
+$AgentLog | Add-Member NoteProperty AgentGUID "Could not find an Agent GUID"
+$AgentLog | Add-Member NoteProperty VaultGUID "Could not find a Vault GUID"
 
 #, vid=4e354d4a-4d7b-49d7-8c9d-11de84e19bff, cid=9c269aa4-ee11-491c-956e-b076a507719a, tid=96336c5c-4aff-4485-a3b0-ca2f31499484
 
@@ -72,9 +63,9 @@ $AgentLog.LogPath = $log1[1].PSPath
 $AgentLog.LogName = $log1[1].PSChildName
 
 # key0 : line identifier key1 : RegEx Expression Matching for key0 identifier key2 is the PSObject Property Name associated with key0 identifier
-$A0 = @{key0 = '(^)((\d{2}\-\w{3}))';key1 = '(^)(?<RegExMatch>(\d{2}\-\w{3}\s\d{2}:\d{2}:\d{2}))';key2 = "TimeStamp"}  # '(^)(?<RegExMatch>(\d{2}\-\w{3}\s\d{2}:\d{2}:\d{2}))' matching format '04-Dec 21:30:02'
-$A1 = @{key0 = "-I-04314";key1 = '(\s)(?<RegExMatch>(\d{1}\.\d{2}\.\d{4}))';key2 = "AgentVersion"}  # " BKUP-I-04314" same code as" REST-I-04314" so chnaging it to "-I-04314" only
-$A2 = @{key0 = "-I-04315";key1 = '(\s)(?<RegExMatch>(\d{1}\.\d{2}))';key2 = "VaultVersion"}  # changed keyword " Vault Version" to " BKUP-I-04315" as in French it would Be "Version du vault" , note sub-filtering by ault As vault in english is upppercase V
+$A0 = @{key0 = '(^)((\d{2}\-\w{3}))';key1 = '(^)(?<RegExMatch>(\d{2}\-\w{3}\s\d{2}:\d{2}:\d{2}))';key2 = "LogEndTime"}  # '(^)(?<RegExMatch>(\d{2}\-\w{3}\s\d{2}:\d{2}:\d{2}))' matching format '04-Dec 21:30:02'
+$A1 = @{key0 = "\-I\-04314";key1 = '(\s)(?<RegExMatch>(\d{1}\.\d{2}\.\d{4}))';key2 = "AgentVersion"}  # " BKUP-I-04314" same code as" REST-I-04314" so chnaging it to "-I-04314" only
+$A2 = @{key0 = "\-I\-04315";key1 = '(\s)(?<RegExMatch>(\d{1}\.\d{2}))';key2 = "VaultVersion"}  # changed keyword " Vault Version" to " BKUP-I-04315" as in French it would Be "Version du vault" , note sub-filtering by ault As vault in english is upppercase V
 $A3 = @{key0 =  " hn=";key1 =  '(hn=)(?<RegExMatch>(.*?))[,\s]\s*';key2 = "HostName"} # RegEx tested on http://rubular.com/ (.*?) where "?" means relunctant (matches only once) as oppose to greedy. See http://groovy.codehaus.org/Tutorial+5+-+Capturing+regex+groups> 
 $A4 = @{key0 =  " ip=";key1 = '(ip=)(?<RegExMatch>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))';key2 = "IPAddress"} # '(ip=)' is not needed here but it looks consistent with previous (hn=)
 $A5 = @{key0 =  " tn=";key1 =  '(tn=)(?<RegExMatch>(.*?))[,\s]\s*';key2 = "TaskName"}
@@ -98,12 +89,16 @@ $A8
 )
 
 for($counter = 0; $counter -lt $Keys.Length; $counter++){
-	$log1 | Where-Object {$_ -match $Keys[$counter].key0 } | ForEach-Object -Process {
-		$_ -match  $Keys[$counter].key1
+	# $consume consumes the result of the pipe since we are not directly interested by the pipe result but its side product from the $matches automatic variable and if true or false match for the conditional if loop
+	$consume = $log1 | Where-Object {$_ -match $Keys[$counter].key0 } | Where-Object {$_ -match  $Keys[$counter].key1}
+
+# key2 Property (like AgentVersion, HostName, TaskName. This is the resulte of observer redundancies and size optimzation of the code
+
+if ($consume -notlike $null) {
 		$temp = $Keys[$counter].key2
-		$Proper = $temp + $Matches.Count		# this only provide the total count of matching object here 4 since there are 4 backup logs in 1
-		$AgentLog | Add-Member NoteProperty "$Proper" $Matches[2]
-	}
+		$AgentLog."$temp" = $Matches[2]
+}
+
 }
 
 # Use case
@@ -114,6 +109,7 @@ $AgentLog | Set-Clipboard
 #
 #LogPath      : C:\posh\input\BACKUP.XLOG.log
 #LogName      : BACKUP.XLOG.log
+#LogEndTime   : 12-Dec 21:33:48
 #AgentVersion : 7.24.9012
 #VaultVersion : 7.01
 #HostName     : Host-1
@@ -122,5 +118,4 @@ $AgentLog | Set-Clipboard
 #TaskGUID     : 96336c5c-4aff-4485-a3b0-ca2f31499484
 #AgentGUID    : 9c269aa4-ee11-491c-956e-b076a507719a
 #VaultGUID    : 4e354d4a-4d7b-49d7-8c9d-11de84e19bff
-
 

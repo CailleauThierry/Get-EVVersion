40c7ba5ca12ae4285299d350c765736a24e65851
CailleauThierry <thierrycailleau@yahoo.fr>
2020-09-07 18:13:27 +0200

    updating regex rules for Director 8.50

--- Get-EVVersion.ps1
+++ Get-EVVersion.ps1
@@ -83,6 +83,32 @@ $A8 = @{key0 = " cid=";key1 = '(cid=)(?<RegExMatch>(\w{8}\-\w{4}\-\w{4}\-\w{4}\-
 $A9 = @{key0 = " vid=";key1 = '(vid=)(?<RegExMatch>(\w{8}\-\w{4}\-\w{4}\-\w{4}\-\w{12}))';key2 = "VaultGUID"}  				# since guid are a set format, I do not need to match the "," at the end
 
 } 
+elseif (($log1[1]) -match '(^)((\d{2}\-\w{3})-)') {
+	# matching Vault 8.50 log formating
+	#Log object definition also defines in what order the objects will be displayed at the end
+	$Log | Add-Member NoteProperty LogEndTime "Could not find a valid time format for an Agent log in text format"
+	$Log | Add-Member NoteProperty AgentVersion "No Agent version Available in this log"
+	$Log | Add-Member NoteProperty VaultVersion "No Vault version Available in this log"
+	$Log | Add-Member NoteProperty HostName "Could not find a Host name"
+	$Log | Add-Member NoteProperty IPAddress "Could not find a IP Address"
+	$Log | Add-Member NoteProperty TaskName "Could not find a task name"
+	$Log | Add-Member NoteProperty TaskNumber "Could not find a TaskNumber number" # This Agent specific parameter is the reason why the whole $Log object definition is repeated below
+	$Log | Add-Member NoteProperty TaskGUID "Could not find a Task GUID"
+	$Log | Add-Member NoteProperty AgentGUID "Could not find an Agent GUID"
+	$Log | Add-Member NoteProperty VaultGUID "Could not find a Vault GUID"
+	
+	# key0 : line identifier key1 : RegEx Expression Matching for key0 identifier key2 is the PSObject Property Name associated with key0 identifier
+	$A0 = @{key0 = '(^)((\d{2}\-\w{3})-)';key1 = '(^)(?<RegExMatch>(\d{2}-\w{3,4}-\d{4}\s\d{2}:\d{2}:\d{2}))';key2 = "LogEndTime"}  	# '(^)(?<RegExMatch>(\w{3,4}\d{2}\s\d{2}:\d{2}:\d{2}))' matching format 'May22 21:19:03'
+	$A1 = @{key0 = "\-I\-0354";key1 = '(\<)(?<RegExMatch>(\d{1}\.\d{2}\.\d{4}))';key2 = "AgentVersion"}  					# '(\<)(?<RegExMatch>(\d{1}\.\d{2}\.\d{4}))' matching format 'VVLT-I-0354 Agent version is <7.50.6422>'
+	$A2 = @{key0 = "\-I\-0219";key1 = '(\s)(?<RegExMatch>(\d{1}\.\d{2}))';key2 = "VaultVersion"}  							# changed keyword " Vault Version" to " BKUP-I-04315" as in French it would Be "Version du vault" , note sub-filtering by ault As vault in english is upppercase V
+	$A3 = @{key0 =  "hn =";key1 = '(hn = )(?<RegExMatch>(.*?))($)';key2 = "HostName"} 										# '(hn =)(?<RegExMatch>(.*?))($)' matching format 'hn = 1_host_name 
+	$A4 = @{key0 =  "ip =";key1 = '(ip = )(?<RegExMatch>(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))';key2 = "IPAddress"} 			# '(ip=)' is not needed here but it looks consistent with previous (hn=)
+	$A5 = @{key0 =  " tn = ";key1 = '(tn = )(?<RegExMatch>(.*))';key2 = "TaskName"}											# '(tn = )(?<RegExMatch>(.*))' matching format 'tn = 1_task_name'
+	$A6 = @{key0 =  " tid= ";key1 = '(tid= .*  \()(?<RegExMatch>([\d]{1,9}))';key2 = "TaskNumber"}							# '((tid= .*  \()(?<RegExMatch>([\d]{1,9}))' matching format 'tid= e69994fd-fd03-4a15-b645-0c7097760595  (2)' by counting 1 "single space" i.e '[\s]' and then 2 single space i.e. [\s]{2} then checking for opening parentheses '\('  where '.*' stands for "anycharacters in between"
+	$A7 = @{key0 = " tid=";key1 = '(tid= )(?<RegExMatch>(\w{8}\-\w{4}\-\w{4}\-\w{4}\-\w{12}))';key2 = "TaskGUID"}
+	$A8 = @{key0 = " cid=";key1 = '(cid= )(?<RegExMatch>(\w{8}\-\w{4}\-\w{4}\-\w{4}\-\w{12}))';key2 = "AgentGUID"}  				# there is no "," at the end of the first cid
+	$A9 = @{key0 = " vid=";key1 = '(vid= )(?<RegExMatch>(\w{8}\-\w{4}\-\w{4}\-\w{4}\-\w{12}))';key2 = "VaultGUID"}  				# since guid are a set format, I do not need to match the "," at the end
+	}
 else
 {
 # matching Vault 7.11 log formating
